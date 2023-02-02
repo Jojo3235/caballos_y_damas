6d51493dacbe04283a38af17d3ced6945e396619
Jojo3235 <joralmand@gmail.com>
2023-02-02 10:23:05 +0100

    limite damas

---
+++ damas/pruebas.py
@@ -0,0 +1,29 @@
+def pedir_numero(cond):
+    while True:
+        try:
+            numero = int(input(cond))
+            break
+        except ValueError:
+            print("No es un numero")
+    return numero
+
+def posición_damas(n):
+    lista_pares = []
+    lista_impares = []
+    if n <= 1000000:
+        for i in range(n):
+            if (i+1)%2 == 0:
+                lista_pares.append((i))
+            else:
+                lista_impares.append((i)) 
+        lista_final = lista_pares + lista_impares
+        return lista_final
+    else:
+        raise OverflowError("El número excede el límite permitido")
+
+def main():
+    n = pedir_numero("Introduce el numero de casillas(<1000000): ")
+    print(posición_damas(n))
+
+if __name__ == "__main__":
+    main()
